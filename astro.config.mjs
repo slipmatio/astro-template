47e22c07b7f33edb0d73613b060f3ade3d306da8
Ville Säävuori <ville@unessa.net>
2025-03-02 16:31:45 +0200

    chore: bump all deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,55 +1,51 @@
-import tailwind from '@astrojs/tailwind'
-import vue from '@astrojs/vue'
-import { unheadVueComposablesImports } from '@unhead/vue'
-import { defineConfig } from 'astro/config'
-import { fileURLToPath, URL } from 'node:url'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import PackageJson from './package.json' with { type: 'json' }
+import vue from "@astrojs/vue";
+import tailwind from "@tailwindcss/vite";
+import { unheadVueComposablesImports } from "@unhead/vue";
+import { defineConfig } from "astro/config";
+import { fileURLToPath, URL } from "node:url";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import PackageJson from "./package.json" with { type: "json" };
 
-process.env.VITE_APP_VERSION = PackageJson.version
-if (process.env.NODE_ENV === 'production') {
-  process.env.VITE_APP_BUILD_EPOCH = new Date().getTime().toString()
+process.env.VITE_APP_VERSION = PackageJson.version;
+if (process.env.NODE_ENV === "production") {
+	process.env.VITE_APP_BUILD_EPOCH = new Date().getTime().toString();
 }
 
 // https://astro.build/config
 export default defineConfig({
-  prefetch: {
-    prefetchAll: true,
-    defaultStrategy: 'viewport',
-  },
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    vue({ appEntrypoint: '/src/app.ts', devtools: true }),
-  ],
-  vite: {
-    plugins: [
-      AutoImport({
-        imports: [
-          'vue',
-          'vue-router',
-          'pinia',
-          {
-            '@/store': ['useStore'],
-          },
-          unheadVueComposablesImports,
-        ],
-        dts: 'auto-imports.d.ts',
-        vueTemplate: true,
-      }),
-      Components({
-        dts: 'components.d.ts',
-      }),
-    ],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
-      },
-    },
-    css: {
-      preprocessorMaxWorkers: true,
-    },
-  },
-})
+	prefetch: {
+		prefetchAll: true,
+		defaultStrategy: "viewport",
+	},
+	integrations: [vue({ appEntrypoint: "/src/app.ts", devtools: true })],
+	vite: {
+		plugins: [
+			tailwind(),
+			AutoImport({
+				imports: [
+					"vue",
+					"vue-router",
+					"pinia",
+					{
+						"@/store": ["useStore"],
+					},
+					unheadVueComposablesImports,
+				],
+				dts: "auto-imports.d.ts",
+				vueTemplate: true,
+			}),
+			Components({
+				dts: "components.d.ts",
+			}),
+		],
+		resolve: {
+			alias: {
+				"@": fileURLToPath(new URL("./src", import.meta.url)),
+			},
+		},
+		css: {
+			preprocessorMaxWorkers: true,
+		},
+	},
+});
